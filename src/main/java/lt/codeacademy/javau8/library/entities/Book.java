b0b45fe72ee969485782f17a353c51df4f975651
Vaidas <mano@mailas.lt>
2024-01-15 08:40:50 +0200

    Created Book Entity

---
+++ src/main/java/lt/codeacademy/javau8/library/entities/Book.java
@@ -0,0 +1,72 @@
+package lt.codeacademy.javau8.library.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	Long id;
+	
+	String title;
+	String author;
+	String year;
+	
+	public Book() {}
+
+	public Book(Long id, String title, String author, String year) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+	
+	public Book(String title, String author, String year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return title + ", " + author + "[" + year + "]";
+	}
+	
+	
+}
